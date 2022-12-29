d95afd2fd867efc20ffe1933318cf8bb7e35b131
unknown <imdlsrks.mc@gmail.com>
2022-12-29 19:28:08 +0900

    DD

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,35 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using DG.Tweening;
 
 public class Card : MonoBehaviour
 {
-    [SerializeField] Sprite frontbackground;  // 카드 앞면 배경
-    [SerializeField] Sprite backbackground;   // 카드 뒷면 배경
-    [SerializeField] Renderer tcgimage;         // 카드 이미지
-
-
-    public TextMesh cardname;       // 카드 이름
-    public TextMesh description;    // 카드 설명
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        SetOrder();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    //레이어 순서 지정
-    public void SetOrder() 
-    {
+    [SerializeField] Renderer frontBackgroud;
+    [SerializeField] Renderer backBackground;
+    [SerializeField] TMP_Text cardname;
+    [SerializeField] TMP_Text descriptcion;
 
 
 
-    }
+
 }
