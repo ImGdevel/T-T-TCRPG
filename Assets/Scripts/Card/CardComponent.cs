4f15adc02ffc3ac45875ce0558e5e8d8d8e1d26a
우승화 <imdlsrks.mc@gmail.com>
2023-06-30 23:54:00 +0900

    23/06/30 - CardComponent Update
     CardComponent class modify and update
    1. spelling correction
    2. add comment
    3. Delete unnecessary functions and references
    
    CardComponent 클래스 수정 및 업데이트
    1. 맞춤법 수정
    2. 코멘트 추가
    3. 불필요한 함수 및 참조를 삭제

--- Assets/Scripts/Card/CardComponent.cs
+++ Assets/Scripts/Card/CardComponent.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.UIElements;
 using UnityEngine;
@@ -7,46 +6,51 @@ using TMPro;
 
 public class CardComponent : MonoBehaviour
 {
-    [SerializeField] Renderer frontBackgroud;
+    [SerializeField] Renderer frontBackground;
     [SerializeField] Renderer backBackground;
     [SerializeField] SpriteRenderer cardSprite;
     [SerializeField] TMP_Text cardNameText;
-    [SerializeField] TMP_Text cardDescriptcionText;
+    [SerializeField] TMP_Text cardDescriptionText;
 
-    [SerializeField] Renderer[] cardRanderers;
-    [SerializeField] Renderer[] textRanderers;
+    [SerializeField] Renderer[] cardRenderers;
+    [SerializeField] Renderer[] textRenderers;
 
     bool doAnimation;
     Card cardInfo;
 
-    public void Start() {
-        
-    }
-
     public void SortingCardLayers(int order) {
         int orderPoint = (order * 3) + 1;
 
-        frontBackgroud.sortingOrder = orderPoint;
+        frontBackground.sortingOrder = orderPoint;
         cardSprite.sortingOrder = orderPoint - 1;
 
-        foreach (var render in cardRanderers) {
-            render.sortingLayerName = "card";
-            render.sortingOrder = orderPoint;
+        foreach (var renderer in cardRenderers) {
+            renderer.sortingLayerName = "card";
+            renderer.sortingOrder = orderPoint;
         }
 
-        foreach (var render in textRanderers) {
-            render.sortingLayerName = "card";
-            render.sortingOrder = orderPoint + 1;
+        foreach (var renderer in textRenderers) {
+            renderer.sortingLayerName = "card";
+            renderer.sortingOrder = orderPoint + 1;
         }
     }
 
-    // card setting
+    /// <summary>
+    /// 카드 정보를 설정합니다.
+    /// </summary>
+    /// <param name="card">설정할 카드 정보</param>
     public void Setup(Card card) {
         cardInfo = card;
         cardNameText.text = card.name;
         cardSprite.sprite = card.sprite;
     }
 
+    /// <summary>
+    /// 카드를 이동시킵니다.
+    /// </summary>
+    /// <param name="movePoint">이동할 위치, 회전, 크기 정보</param>
+    /// <param name="useDOTween">DOTween을 사용하여 애니메이션을 실행할지 여부</param>
+    /// <param name="time">애니메이션에 걸리는 시간</param>
     public void MoveTransform(PRS movePoint, bool useDOTween, float time = 1) {
         var pos = movePoint.position;
         var rot = movePoint.rotation;
