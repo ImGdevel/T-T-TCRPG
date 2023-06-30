65f0eae39408b20270208790abd9a12c0735fd01
우승화 <imdlsrks.mc@gmail.com>
2023-06-30 18:26:53 +0900

    23/06/30-Character Class
    Character Class
    Parent class of Hero class and Enemy class

---
+++ Assets/Scripts/Battle/Unit/Character.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character
+{
+    protected string name;
+    protected int maxHealth;
+    protected int currentHealth;
+    protected int attackPower;
+    protected BuffManager buffManager;
+
+    public string Name { get { return name; } }
+    public int MaxHealth { get { return maxHealth; } }
+    public int CurrentHealth { get { return currentHealth; } }
+    public int AttackPower { get { return attackPower; } }
+    public BuffManager BuffManager { get { return buffManager; } }
+
+    public abstract void Attack();
+    public abstract void Defend();
+    public abstract void TakeDamage(int damage);
+    public abstract void Die();
+}
