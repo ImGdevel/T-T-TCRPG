3b9df33d3e25157d553bb3ab3e365c0ec39a786a
우승화 <imdlsrks.mc@gmail.com>
2023-06-30 18:30:03 +0900

    23/06/30 - Hero SubClass
    Hero SubClass
    Warrior and Mage

---
+++ Assets/Scripts/Battle/Unit/Heros/Warrior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior : Hero
+{
+    public Warrior(string name, int level, int maxHealth, int attackPower)
+        : base(name, HeroClassType.Warrior, level, maxHealth, attackPower) {
+        // 추가적인 초기화 코드
+    }
+
+    public override void Attack() {
+        // Warrior의 공격 로직 구현
+        // 예: 근접 공격 실행
+    }
+
+    public override void Defend() {
+        // Warrior의 방어 로직 구현
+        // 예: 방어 태세 전환
+    }
+}
