daf561dd55e010c700dab649f4b7928cd96295e3
우승화 <imdlsrks.mc@gmail.com>
2023-07-01 00:26:01 +0900

    Revert "Merge branch 'battle' of https://github.com/ImDevelo/T-T-TCRPG into battle"
    This reverts commit b4ddb57f0e9fd56a69d0c5892a29fd18487c6b36, reversing
    changes made to e20fb7e0226d32eddabb56f854b294112ef7437b.

--- Assets/Scripts/Card/CardComponent.cs
+++ Assets/Scripts/Card/CardComponent.cs
@@ -25,9 +25,9 @@ public abstract class CardComponent : MonoBehaviour
     public abstract void OnMouseDown();
 
     /// <summary>
-    /// Ä«ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Õ´Ï´ï¿½.
+    /// Ä«µå Á¤º¸¸¦ ¼³Á¤ÇÕ´Ï´Ù.
     /// </summary>
-    /// <param name="card">ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ Ä«ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½</param>
+    /// <param name="card">¼³Á¤ÇÒ Ä«µå Á¤º¸</param>
     public void Setup(Card card) {
         cardInfo = card;
         cardNameText.text = card.name;
@@ -35,11 +35,11 @@ public abstract class CardComponent : MonoBehaviour
     }
 
     /// <summary>
-    /// Ä«ï¿½å¸¦ ï¿½Ìµï¿½ï¿½ï¿½Åµï¿½Ï´ï¿½.
+    /// Ä«µå¸¦ ÀÌµ¿½ÃÅµ´Ï´Ù.
     /// </summary>
-    /// <param name="movePoint">ï¿½Ìµï¿½ï¿½ï¿½ ï¿½ï¿½Ä¡, È¸ï¿½ï¿½, Å©ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½</param>
-    /// <param name="useDOTween">DOTweenï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Ï¿ï¿½ ï¿½Ö´Ï¸ï¿½ï¿½Ì¼ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½</param>
-    /// <param name="time">ï¿½Ö´Ï¸ï¿½ï¿½Ì¼Ç¿ï¿½ ï¿½É¸ï¿½ï¿½ï¿½ ï¿½Ã°ï¿½</param>
+    /// <param name="movePoint">ÀÌµ¿ÇÒ À§Ä¡, È¸Àü, Å©±â Á¤º¸</param>
+    /// <param name="useDOTween">DOTweenÀ» »ç¿ëÇÏ¿© ¾Ö´Ï¸ÞÀÌ¼ÇÀ» ½ÇÇàÇÒÁö ¿©ºÎ</param>
+    /// <param name="time">¾Ö´Ï¸ÞÀÌ¼Ç¿¡ °É¸®´Â ½Ã°£</param>
     public void MoveTransform(PRS movePoint, bool useDOTween, float time = 1) {
         var pos = movePoint.position;
         var rot = movePoint.rotation;
@@ -58,9 +58,9 @@ public abstract class CardComponent : MonoBehaviour
     }
 
     /// <summary>
-    /// Ä«ï¿½ï¿½ ï¿½ï¿½ï¿½Ì¾ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Õ´Ï´ï¿½.
+    /// Ä«µå ·¹ÀÌ¾î ¼ø¼­¸¦ ÁöÁ¤ÇÕ´Ï´Ù.
     /// </summary>
-    /// <param name="order"> Ä«ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½È£</param>
+    /// <param name="order"> Ä«µå °íÀ¯ ¼ø¼­ ¹øÈ£</param>
     public void SortingCardLayers(int order) {
         int orderPoint = (order * 3) + 1;
 
