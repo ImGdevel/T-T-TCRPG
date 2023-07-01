c3f005ff33c492c0a2ac0f7457f716172eaaa270
SH Woo <imdlsrks.mc@gmail.com>
2023-07-01 15:52:01 +0900

    Card ScriptableObject Path Update

--- Assets/Scripts/Card/CardData.cs
+++ Assets/Scripts/Card/CardData.cs
@@ -11,21 +11,21 @@ public abstract class CardData : ScriptableObject
 }
 
 
-[CreateAssetMenu(fileName = "New Battle Card", menuName = "Card/Battle Card")]
+[CreateAssetMenu(fileName = "Battle Card", menuName = "Card/Battle Card")]
 public class BattleCardData : CardData
 {
     public int cost;
     public CardEffect[] effects;
 }
 
-[CreateAssetMenu(fileName = "New Exploration Card", menuName = "Card/Exploration Card")]
+[CreateAssetMenu(fileName = "Exploration Card", menuName = "Card/Exploration Card")]
 public class ExplorationCardData : CardData
 {
     public int requiredSteps;
     public CardEffect[] effects;
 }
 
-[CreateAssetMenu(fileName = "New Artifact Card", menuName = "Card/Artifact Card")]
+[CreateAssetMenu(fileName = "Artifact Card", menuName = "Card/Artifact Card")]
 public class ArtifactCardData : CardData
 {
     public CardEffect[] effects;
