a17505319aaa973aaacc0dda5e1df1ca131bd7b0
SH Woo <imdlsrks.mc@gmail.com>
2023-07-09 15:01:39 +0900

    Update HandCardComponent.cs
    add Card State and add comment

--- Assets/Scripts/Card/Card Component/HandCardComponent.cs
+++ Assets/Scripts/Card/Card Component/HandCardComponent.cs
@@ -7,28 +7,34 @@ using TMPro;
 
 public class HandCardComponent : CardComponent
 {
-    HandManager handManager;
-    PRS originPosition;
-    int originSortingLayer;
-    bool activeHandOverAni = true;
-    bool isSelected = false;
+    private HandManager handManager; // 핸드 메니저
+    private PRS originPosition; // 원래 Transform 위치
+    private int originSortingLayer; // 원래 카드 번호
+    private bool activeHandOverAni = true; // 카드 애니메이션 작동 여부
+    private bool isSelected = false; // 카드 선택 여부
 
-    /// <summary>
-    /// 매 프레임마다 호출되는 메서드
-    /// </summary>
-    void Update() {
+    public bool IsCardEnabled {
+        set { isMouseClick = value; }
+        get { return isMouseClick; }
+    } // 카드 활성화 여부
 
-        if (isSelected) {
-            // 카드 선택 동작 처리
-            if (Input.GetMouseButton(1)) {
-                // 카드를 놓음
-                UnselectCard();
-            }
-            else {
-                // 마우스 위치에 카드 이동
-                this.transform.position = Utills.MousePointer;
-            }
+    private void Start() {
+        IsCardEnabled = false;
+    }
+
+    private void Update() {
+        if (!isSelected) return;
+        
+        // 카드 선택 동작 처리
+        if (Input.GetMouseButton(1)) {
+            // 카드를 놓음
+            UnselectCard();
         }
+        else {
+            // 마우스 위치에 카드 이동
+            this.transform.position = Utills.MousePointer;
+        }
+
     }
 
     /// <summary>
@@ -60,8 +66,7 @@ public class HandCardComponent : CardComponent
     /// 마우스가 카드에 들어갈 때 호출되는 메서드
     /// </summary>
     public override void OnMouseEnter() {
-        
-        if (!activeHandOverAni || isSelected) return;
+        if (!isMouseOver || !activeHandOverAni || isSelected) return;
 
         Vector3 pos = new Vector3(transform.position.x, transform.parent.position.y + 1, -5);
         PRS prs = new PRS(pos, Quaternion.identity, Vector3.one * 1.3f);
@@ -73,8 +78,7 @@ public class HandCardComponent : CardComponent
     /// 마우스가 카드에서 벗어날 때 호출되는 메서드
     /// </summary>
     public override void OnMouseExit() {
-        
-        if (!activeHandOverAni || isSelected) return;
+        if (!isMouseOver || !activeHandOverAni || isSelected) return;
 
         MoveTransform(originPosition, true, 0.1f);
         SortingCardLayers(originSortingLayer);
@@ -84,6 +88,7 @@ public class HandCardComponent : CardComponent
     /// 마우스로 카드를 클릭했을 때 호출되는 메서드
     /// </summary>
     public override void OnMouseDown() {
+        if (!isMouseClick) return;
 
         if (!isSelected && !handManager.isCardSelected) {
             isSelected = true;
@@ -96,7 +101,7 @@ public class HandCardComponent : CardComponent
     }
 
     /// <summary>
-    /// 카드 선택 해제 메서드
+    /// 카드 선택 해제
     /// </summary>
     public void UnselectCard() {
         isSelected = false;
@@ -106,7 +111,7 @@ public class HandCardComponent : CardComponent
     }
 
     /// <summary>
-    /// 카드 사용 메서드
+    /// 카드 사용
     /// </summary>
     public void UseCard() {
         Debug.Log("카드 사용");
@@ -114,4 +119,21 @@ public class HandCardComponent : CardComponent
         handManager.isCardSelected = false;
         handManager.UseCardRemove(this.gameObject);
     }
+
+    /// <summary>
+    /// 카드 사용 활성화
+    /// </summary>
+    public void EnableCard() {
+        IsCardEnabled = true;
+        // 카드 사용 활성화 애니메이션
+    }
+
+    /// <summary>
+    /// 카드 사용 비활성화
+    /// </summary>
+    public void DisableCard() {
+        IsCardEnabled = false;
+        // 카드 사용 비 활성화 애니메이션
+    }
+
 }
