5eee23ce270d25d589a65e500a156db05cefa9ca
SH Woo <imdlsrks.mc@gmail.com>
2023-07-09 15:11:47 +0900

    add Comment in HandManager

--- Assets/Scripts/Card/Hand and Deck/HandManager.cs
+++ Assets/Scripts/Card/Hand and Deck/HandManager.cs
@@ -12,35 +12,37 @@ public class HandManager : MonoBehaviour
     [SerializeField] List<Card> decks;
     [SerializeField] List<GameObject> hands;
 
-    // Hand Setting
-    [SerializeField] int maxHandSize; // maximum hand card
+    // Hand 설정
+    [SerializeField] int maxHandSize; // 최대 손 카드 개수
 
-    // selcted HandCard
+    // 선택된 Hand 카드
     private GameObject selectedCard;
-    public bool isCardSelected = false; 
+    public bool isCardSelected = false;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         handsPoint = this.transform;
-        decks = Mydeck.RandomDeck(100); //덱을 가져옵니다.
+        decks = Mydeck.RandomDeck(100); // 덱을 가져옵니다.
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
             DrowCard();
         }
     }
 
+    /// <summary>
+    /// 덱에서 아이템(카드)을 가져옵니다.
+    /// </summary>
+    /// <returns> 가져온 카드 </returns>
     Card PopItem() {
         Card card = decks[0];
         decks.RemoveAt(0);
         return card;
     }
 
+    /// <summary>
+    /// 덱에서 카드를 뽑아 플레이어의 손에 추가합니다.
+    /// </summary>
     void DrowCard() {
         if (maxHandSize <= hands.Count) return;
 
@@ -52,8 +54,11 @@ public class HandManager : MonoBehaviour
         SortingCard();
     }
 
+    /// <summary>
+    /// 플레이어의 손에 있는 카드를 정렬합니다.
+    /// </summary>
     public void SortingCard() {
-        for(int i=0; i< hands.Count; i++) {
+        for (int i = 0; i < hands.Count; i++) {
             var cardComponet = hands[i].GetComponent<HandCardComponent>();
             var pos = RadiansPRS(i);
             cardComponet.MoveTransform(pos, true, 0.5f);
@@ -63,50 +68,71 @@ public class HandManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// 인덱스에 따라 플레이어의 손에 있는 카드의 위치, 회전 및 크기를 계산합니다.
+    /// </summary>
+    /// <param name="number">카드의 인덱스 순서 번호</param>
+    /// <returns>카드의 위치, 회전 및 크기 정보인 PRS(PRS(Position, Rotation, Scale)) 값</returns>
     PRS RadiansPRS(int number) {
         float gap = 1;
         float slope = 1;
         float radius = 25;
         float count = hands.Count;
-        float MaxWidth = handsPoint.position.x - ((count-1) / 2  * gap);
+        float MaxWidth = handsPoint.position.x - ((count - 1) / 2 * gap);
         float Xpos = MaxWidth + number * gap;
-        float Ypos = this.transform.position.y + UMath.Base(radius, Xpos) 
-                     - UMath.Base(radius, MaxWidth) - (radius - UMath.Base(radius, MaxWidth)) / 2;
+        float Ypos = this.transform.position.y + UMath.Base(radius, Xpos) - UMath.Base(radius, MaxWidth) - (radius - UMath.Base(radius, MaxWidth)) / 2;
         var rot = Quaternion.Slerp(Quaternion.Euler(0, 0, count * slope), Quaternion.Euler(0, 0, -count * slope), 1 / count * number);
-        
+
         return new PRS(new Vector3(Xpos, Ypos), rot, Vector3.one);
     }
 
+    /// <summary>
+    /// 플레이어의 손에 있는 카드를 선택합니다.
+    /// </summary>
+    /// <param name="card">선택된 카드 오브젝트</param>
     public void SelectCard(GameObject card) {
-        //현재 선택된 카드
+        // 이미 카드가 선택된 상태라면 무시합니다.
         if (isCardSelected) return;
+
         isCardSelected = true;
     }
 
-   
+    /// <summary>
+    /// 사용된 카드를 플레이어의 손에서 제거합니다.
+    /// </summary>
+    /// <param name="card">제거할 카드</param>
     public void UseCardRemove(GameObject card) {
-        //사용된 카드 핸드에서 삭제
+        // 사용된 카드를 손에서 제거합니다.
         Destroy(card, .1f);
         hands.Remove(card);
-        
+
         SortingCard();
     }
 
+    /// <summary>
+    /// 플레이어의 손에 있는 모든 카드를 사용 가능하도록 활성화합니다.
+    /// </summary>
     public void EnableAllCards() {
-        // 모든 카드 사용 활성화
+        // 모든 카드를 사용 가능하도록 활성화합니다.
         for (int i = 0; i < hands.Count; i++) {
             var cardComponet = hands[i].GetComponent<HandCardComponent>();
             cardComponet.isMouseClick = true;
         }
     }
 
+    /// <summary>
+    /// 조건에 맞는 카드만 플레이어의 손에서 사용 가능하도록 활성화합니다.
+    /// </summary>
     public void EnableCardsByCondition() {
-        // 조건에 맞는 카드만 활성화
-        // 인자로 카드 조건을 받고 조건이 성립하면 카드 활성화
+        // 조건에 맞는 카드만 사용 가능하도록 활성화합니다.
+        // 조건을 인자로 받아와 해당 조건이 충족되면 카드를 활성화합니다.
     }
 
+    /// <summary>
+    /// 플레이어의 손에 있는 모든 카드를 사용 불가능하도록 비활성화합니다.
+    /// </summary>
     public void DisableAllCards() {
-        // 모든 카드 사용 비활성화
+        // 모든 카드를 사용 불가능하도록 비활성화합니다.
         for (int i = 0; i < hands.Count; i++) {
             var cardComponet = hands[i].GetComponent<HandCardComponent>();
             cardComponet.isMouseClick = false;
