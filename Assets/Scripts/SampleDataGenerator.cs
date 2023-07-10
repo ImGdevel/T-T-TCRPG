a04519beff7d50230e4689bfd76d929de6622564
SH Woo <imdlsrks.mc@gmail.com>
2023-07-10 15:15:30 +0900

    SampleDeck

--- Assets/Scripts/SampleDataGenerator.cs
+++ Assets/Scripts/SampleDataGenerator.cs
@@ -5,4 +5,19 @@ using UnityEngine;
 public class SampleDataGenerator
 {
 
+    public List<Card> sampleCardList;
+
+    public static Deck SampleDeck
+    {
+        get {
+            Deck deck = new Deck();
+
+            
+
+
+            return deck;
+        }
+    }
+   
+
 }
