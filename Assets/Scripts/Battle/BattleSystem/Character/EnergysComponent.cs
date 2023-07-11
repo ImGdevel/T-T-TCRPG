a29e5b18c8c4c7bdc97497774c308933edad4be6
SH Woo <imdlsrks.mc@gmail.com>
2023-07-11 20:28:06 +0900

    Energy 04

--- Assets/Scripts/Battle/BattleSystem/Character/EnergysComponent.cs
+++ Assets/Scripts/Battle/BattleSystem/Character/EnergysComponent.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnergysComponent : MonoBehaviour
+public class EnergysComponet : MonoBehaviour
 {
     [SerializeField] EnergyToken[] energyTokens;
 
