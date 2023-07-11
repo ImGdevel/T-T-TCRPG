f677156c915038c33a35312aa9d91830308c8f7e
SH Woo <imdlsrks.mc@gmail.com>
2023-07-11 20:47:15 +0900

    bar componet

--- Assets/Scripts/Battle/BattleSystem/Character/HpBarComponent.cs
+++ Assets/Scripts/Battle/BattleSystem/Character/HpBarComponent.cs
@@ -11,52 +11,46 @@ public class HpBarComponent : MonoBehaviour
     Vector3 barStartPoint;
 
     private float max = 1.0f;
-    private float point = 1.0f;
+    private float current = 1.0f;
 
     private void Start() {
-        if (background == null) {
-            Debug.LogError("The background of the HpBar component does not exist.");
+        if (background == null || fill == null) {
+            Debug.LogError("Background or fill GameObject is not assigned in HpBarComponent.");
+            return;
         }
-        if (fill == null) {
-            Debug.LogError("The background of the HpBar component does not exist.");
-        }
-        else {
 
-            originPos = new PRS(fill.transform.position, fill.transform.rotation, fill.transform.localScale);
-            Renderer renderer = fill.GetComponent<Renderer>();
-            barStartPoint = renderer.bounds.min;
-        }
-        DisplayStatus();
-
-
+        Renderer renderer = fill.GetComponent<Renderer>();
+        originPos = new PRS(fill.transform.position, fill.transform.rotation, fill.transform.localScale);
+        barStartPoint = renderer.bounds.min;
     }
 
     private void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            point -= 0.1f;
-            UpdateStatus(point, 1.0f);
+            current -= 0.1f;
+            UpdateStatus(current, 1.0f);
         }
     }
 
-    public void UpdateStatus(float point, float max = 1.0f) {
-        this.point = point;
+    public void UpdateStatus(float current, float max = 1.0f) {
+        this.current = current;
         this.max = max;
 
         DisplayStatus();
     }
 
     public void DisplayStatus() {
-        PRS currentHpBar = new PRS(originPos.position, originPos.rotation, originPos.scale);
         if (max == 0) {
-            Debug.LogWarning("Max Hp is Zero");
+            Debug.LogWarning("Max HP is zero. Cannot display HP bar.");
             return;
         }
-        float precent = point / max;
-        Renderer renderer = fill.GetComponent<Renderer>();
-        float currentBarLegth = (fill.transform.position.x - renderer.bounds.min.x) * precent;
-        currentHpBar.scale.x = precent * (originPos.scale.x);
-        currentHpBar.position.x = barStartPoint.x + currentBarLegth;
 
-        DoTweenAnimation.MoveTransform(fill.transform, currentHpBar, 0.3f);
+        PRS currentHpBarState = new PRS(originPos.position, originPos.rotation, originPos.scale);
+        Renderer renderer = fill.GetComponent<Renderer>();
+        float precent = current / max;
+
+        currentHpBarState.position.x = barStartPoint.x + (fill.transform.position.x - renderer.bounds.min.x) * precent;
+        currentHpBarState.scale.x *= precent;
+
+        DoTweenAnimation.MoveTransform(fill.transform, currentHpBarState, 0.25f);
     }
 }
