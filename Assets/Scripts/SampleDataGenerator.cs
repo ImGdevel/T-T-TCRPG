99f175d4707ec56f9d61845abbb89e062b786b69
SH Woo <imdlsrks.mc@gmail.com>
2023-07-13 22:48:07 +0900

    Update SampleDataGenerator.cs
    Sample.Character

--- Assets/Scripts/SampleDataGenerator.cs
+++ Assets/Scripts/SampleDataGenerator.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class SampleDataGenerator
 {
 
-    public List<Card> sampleCardList;
 
-    public static Deck SampleDeck
-    {
-        get {
-            Deck deck = new Deck();
+    public List<Character> SAMPLE_CHARACTER() {
 
-            
+        List<Character> sample = new();
 
+        sample.Add(new Warrior("Tain",3,50,10));
+        sample.Add(new Mage("Rora", 4, 30, 20));
 
-            return deck;
-        }
+        return sample;
     }
-   
+
+    public List<Character> SAMPLE_ENEMY() {
+
+        List<Character> sample = new();
+
+        sample.Add(new Enemy("Gole", EnemyClassType.Undead, 50, 10));
+        sample.Add(new Enemy("Ghoul", EnemyClassType.Undead, 30, 20));
+
+        return sample;
+    }
 
 }
