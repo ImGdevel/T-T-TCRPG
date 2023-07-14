765ffe14e978687f4b30c7653c69b3e4a0a8b670
SH Woo <imdlsrks.mc@gmail.com>
2023-07-15 01:13:42 +0900

    Update

--- Assets/Scripts/Card/Card/Card/Card.cs
+++ Assets/Scripts/Card/Card/Card/Card.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Card : MonoBehaviour
+public abstract class Card
 {
     protected CardData cardData;
 
