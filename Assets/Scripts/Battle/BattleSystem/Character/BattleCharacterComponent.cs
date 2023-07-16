8d1df06b1e75cdf5d5f097d45a1a5f54a4b6c88e
SH Woo <imdlsrks.mc@gmail.com>
2023-07-16 17:50:37 +0900

    Connect Card and Character

--- Assets/Scripts/Battle/BattleSystem/Character/BattleCharacterComponent.cs
+++ Assets/Scripts/Battle/BattleSystem/Character/BattleCharacterComponent.cs
@@ -8,10 +8,14 @@ public class BattleCharacterComponent : MonoBehaviour
 
     [SerializeField] BattleStatusComponent statusComponet;
 
+    private Rigidbody2D rb; // Rigidbody2D 컴포넌트 추가
+
     private void Start() {
-        if(statusComponet == null) {
+        if (statusComponet == null) {
             statusComponet = transform.GetComponentInChildren<BattleStatusComponent>();
         }
+
+        rb = GetComponent<Rigidbody2D>(); // Rigidbody2D 컴포넌트 할당
     }
 
     public void SetCharacter(Character character) {
@@ -26,6 +30,20 @@ public class BattleCharacterComponent : MonoBehaviour
         statusComponet.UpdateStatus(character);
     }
 
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Card") {
+            Debug.Log("캐릭터가 닿았습니다.");
+        }
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D other) {
+        // 충돌 상태가 유지되는 동안 호출됩니다.
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        // 충돌이 끝날 때 호출됩니다.
+    }
 }
 
 
