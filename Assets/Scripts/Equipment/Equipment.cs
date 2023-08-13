1e2629eee9496e5e53db76c40a319739cc9d85cf
SH Woo <imdlsrks.mc@gmail.com>
2023-08-14 02:44:47 +0900

    Update Equipment.cs

--- Assets/Scripts/Equipment/Equipment.cs
+++ Assets/Scripts/Equipment/Equipment.cs
@@ -12,13 +12,20 @@ public enum EquipmentType
 [CreateAssetMenu(fileName = "New Equipment", menuName = "Equipment/Equipment")]
 public class Equipment : ScriptableObject
 {
-    public string Name { get; private set; }
-    public EquipmentType Type { get; private set; }
-    public Stats Stats { get; private set; }
+    public string equipmentName;
+    public EquipmentType type;
+    public string description;
+    public Stats stats;
 
-    public Equipment(string name, EquipmentType type, Stats stats) {
-        Name = name;
-        Type = type;
-        Stats = stats;
+    public string Name { get { return equipmentName; } }
+    public EquipmentType Type { get { return type; } }
+    public string Description { get {return description; } }
+    public Stats Stats { get { return stats; } }
+    
+    public Equipment(string name, EquipmentType type, Stats stats, string description = "") {
+        this.equipmentName = name;
+        this.type = type;
+        this.stats = stats;
+        this.description = description;
     }
 }
