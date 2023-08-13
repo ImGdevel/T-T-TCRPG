7e3d9efb50e5f5bda34f653fcf83f85567f099fa
SH Woo <imdlsrks.mc@gmail.com>
2023-08-14 02:44:27 +0900

    Refactory Stats

--- Assets/Scripts/Chracter/Stats.cs
+++ Assets/Scripts/Chracter/Stats.cs
@@ -8,45 +8,94 @@ public enum StatsType
     Energy,
     ShieldPoint,
     AttackPower,
+    Speed,
     // 여기에 추가적인 스탯들을 나열해주세요.
 }
 
+[System.Serializable]
 public class Stats
 {
-    private Dictionary<StatsType, int> stats = new Dictionary<StatsType, int>();
+    public int Health;
+    public int Energy;
+    public int ShieldPoint;
+    public int AttackPower;
+    public int Speed;
+    //private Dictionary<StatsType, int> stats = new Dictionary<StatsType, int>();
+
+    public Stats(int health, int energy, int attackPower, int speed) {
+        Health = health;
+        Energy = energy;
+        ShieldPoint = 0;
+        AttackPower = attackPower;
+        Speed = speed;
+    }
+
+    public Stats(int health, int energy, int shieldPoint, int attackPower, int speed) {
+        Health = health;
+        Energy = energy;
+        ShieldPoint = shieldPoint;
+        AttackPower = attackPower;
+        Speed = speed;
+    }
 
     public Stats(int health, int energy, int attackPower) {
+        Health = health;
+        Energy = energy;
+        ShieldPoint = 0;
+        AttackPower = attackPower;
+        Speed = 1;
+        /*
         stats.Add(StatsType.Health, health);
         stats.Add(StatsType.Energy, energy);
         stats.Add(StatsType.AttackPower, attackPower);
+        stats.Add(StatsType.ShieldPoint, 0);
+        stats.Add(StatsType.Speed, attackPower);
+        */
         // 여기에 추가적인 스탯들을 초기화해주세요.
     }
 
-    // Clone 생성자
-    private Stats(Dictionary<StatsType, int> stats) {
-        this.stats = stats;
-    }
-
     public int GetStat(StatsType type) {
-        if (stats.ContainsKey(type)) {
-            return stats[type];
-        }
-        else {
-            Debug.LogError($"StatsType {type} not found.");
-            return 0;
+        switch (type) {
+            case StatsType.Health:
+                return Health;
+            case StatsType.Energy:
+                return Energy;
+            case StatsType.ShieldPoint:
+                return ShieldPoint;
+            case StatsType.AttackPower:
+                return AttackPower;
+            case StatsType.Speed:
+                return Speed;
+            default:
+                Debug.LogError($"StatsType {type} not found.");
+                return 0;
         }
     }
 
     public void ModifyStat(StatsType type, int amount) {
-        if (stats.ContainsKey(type)) {
-            stats[type] += amount;
-        }
-        else {
-            Debug.LogError($"StatsType {type} not found.");
+        switch (type) {
+            case StatsType.Health:
+                Health += amount; 
+                break;
+            case StatsType.Energy:
+                Energy += amount;
+                break;
+            case StatsType.ShieldPoint:
+                ShieldPoint += amount;
+                break;
+            case StatsType.AttackPower:
+                AttackPower += amount;
+                break;
+            case StatsType.Speed:
+                Speed += amount;
+                break;
+            default:
+                Debug.LogError($"StatsType {type} not found.");
+                break;
         }
     }
 
     public Stats Clone() {
-        return new Stats(stats);
+        return new Stats(Health, Energy, ShieldPoint, AttackPower, Speed);
     }
 }
